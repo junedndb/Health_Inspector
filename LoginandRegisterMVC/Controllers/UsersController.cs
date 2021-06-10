a6aa922494aa4041429519cc7375ef8034650ba7
junedndb <juned.ndb@gmail.com>
2021-06-10 20:35:16 +0530

    Clinic File change

--- LoginandRegisterMVC/Controllers/UsersController.cs
+++ LoginandRegisterMVC/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using LoginandRegisterMVC.Models;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -18,10 +20,18 @@ namespace LoginandRegisterMVC.Controllers
         [Authorize]
         public ActionResult Index(int? id)
         {
-            var adate = (from a in db.Appointments select a).First();
-            TimeSpan span = DateTime.Parse(adate.DateOfAppointment) - DateTime.Today;
-            ViewBag.ADate = span;
+            int num = db.Appointments.Where(a => a.PatientId == id).Count();
 
+            List<Appointment> name = new List<Appointment>();
+
+
+            foreach (var a in db.Appointments.Where(a => a.PatientId == id))
+                {
+                    string span = (DateTime.Parse(a.DateOfAppointment) - DateTime.Today).TotalDays.ToString();
+                    name.Add(a);
+                }
+
+            ViewBag.AName = name;
             Session["Id"] = id;
 /*            var data = (from a in db.Clinics
                         select a).ToList();
@@ -54,7 +64,11 @@ namespace LoginandRegisterMVC.Controllers
             {
                 db.Users.Add(user);
                 db.SaveChanges();
-                return RedirectToAction("Login");
+
+                ViewBag.Message = "New User Registered Successfully !";
+
+                return View();
+//                return RedirectToAction("Login");
 
             }
             return View(user);
@@ -70,40 +84,42 @@ namespace LoginandRegisterMVC.Controllers
         public ActionResult Login(User user)
         {
 
-            var data = (from a in db.Users where a.MailId == user.MailId && a.Password == user.Password  && a.Role == user.Role select a).First();
-//            bool IsValidUser = db.Users.Any(a => a.MailId == user.MailId) && dt.Password == user.Password && role==user.Role ;
-            bool IsValidUser = db.Users.Any(a => a.MailId == user.MailId) && db.Users.Any(a => a.Password == user.Password) && db.Users.Any(a => a.Role == user.Role);
+//            var data = (from a in db.Users where a.MailId == user.MailId && a.Password == user.Password  && a.Role == user.Role select a).First();
+            //            bool IsValidUser = db.Users.Any(a => a.MailId == user.MailId) && dt.Password == user.Password && role==user.Role ;
+            //            bool IsValidUser = db.Users.Where(a => a.MailId == user.MailId) && db.Users.Where(a => a.Password == user.Password) && db.Users.Where(a => a.Role == user.Role);
 
+            var IsValidUser = db.Users.Where(a => a.MailId == user.MailId && a.Password == user.Password && a.Role == user.Role).FirstOrDefault();
 
-            if (IsValidUser)
+            if (IsValidUser!=null)
                 {
                     FormsAuthentication.SetAuthCookie(user.Username, false);
                     Session["MailId"] = user.MailId.ToString();
 
-                    if (data.Role == "Doctor")
+                    if (IsValidUser.Role == "Doctor")
                     {
                         Session["RoleLogin"] = user.Role;
-                        Session["Id"] = data.UserId;
+                        Session["Id"] = IsValidUser.UserId;
 //                        return RedirectToAction("Index", "Doctor",new {id = MailId.UserId});
-                    return RedirectToAction("Index", "Doctor",new { id = data.UserId});
+                    return RedirectToAction("Index", "Doctor",new { id = IsValidUser.UserId});
                     }
-                    else if (data.Role == "Patient")
+                    else if (IsValidUser.Role == "Patient")
                     {
 
-                    Session["Id"] = data.UserId;
+                    Session["Id"] = IsValidUser.UserId;
                     Session["RoleLogin"] = user.Role;
-                        return RedirectToAction("Index", "Users", new { id = data.UserId });
+                        return RedirectToAction("Index", "Users", new { id = IsValidUser.UserId });
                     }
-                    else if (data.Role == "Admin")
+                    else if (IsValidUser.Role == "Admin")
                     {
-                    Session["Id"] = data.UserId;
+                    Session["Id"] = IsValidUser.UserId;
                     Session["RoleLogin"] = user.Role;
-                        return RedirectToAction("Index", "Adminright", new { id = data.UserId });
+                        return RedirectToAction("Index", "Adminright", new { id = IsValidUser.UserId });
                     }
                 }
                 else
                 {
                     ModelState.AddModelError("", "invalid Username or Password or UserType");
+               // return Content("invalid Username or Password or UserType");
 
                 }
             return View();
@@ -152,17 +168,17 @@ namespace LoginandRegisterMVC.Controllers
         {
             //            var comp = (from a in db.Users where a.MailId == user.MailId select a.Password).First();
             //            var comp = db.Users.Where(a => a.MailId == user.MailId).FirstOrDefault();
-            bool IsValid = db.Users.Any(a => a.MailId == user.MailId) &&
-                            db.Users.Any(a => a.Fque == user.Fque) &&
-                            db.Users.Any(a => a.Sque == user.Sque) &&
-                            db.Users.Any(a => a.Tque == user.Tque);
-            if (IsValid)
+            var IsValid = db.Users.Where(a => a.MailId == user.MailId &&
+                        a.Fque == user.Fque && a.Sque == user.Sque &&
+                        a.Tque == user.Tque && a.Role == user.Role).FirstOrDefault();
+            if (IsValid !=null)
             {
-                var comp = (from a in db.Users
-                            where a.MailId == user.MailId &&
-                            a.Fque == user.Fque && a.Sque == user.Sque && a.Tque == user.Tque
-                            select a).FirstOrDefault();
-                comp.Password = user.Password;
+                /*                var comp = (from a in db.Users
+                                            where a.MailId == user.MailId &&
+                                            a.Fque == user.Fque && a.Sque == user.Sque && a.Tque == user.Tque
+                                            select a).FirstOrDefault();
+                */
+                IsValid.Password = user.Password;
                 db.SaveChanges();
                 return RedirectToAction("Login");
             }
